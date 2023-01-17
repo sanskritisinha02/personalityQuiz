8898f824f11bdddbdaef2751bfa37e6cf0584537
Sanskriti Sinha <sanskritisinha.02@gmail.com>
2023-01-17 23:18:32 +0530

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Sanskriti Sinha on 17/01/2023.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
+
